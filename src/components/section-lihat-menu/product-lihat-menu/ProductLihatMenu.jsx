1831123cf92a3d805cfec88e77f7c62746510db9
Ivan Zulyan Pratama <zulyanivan11@gmail.com>
2023-11-27 14:32:25 +0700

    done from van

---
+++ src/components/section-lihat-menu/product-lihat-menu/ProductLihatMenu.jsx
@@ -0,0 +1,120 @@
+import React from "react";
+import MenuSatu from "../../../assets/menu-section/menu-dua.jpg";
+import { StarIcon } from "@heroicons/react/24/solid";
+
+function ProductLihatMenu() {
+  return (
+    <section className="container my-20">
+      <h1 className="text-xl font-semibold ms-4 mb-5">Produk Kuliner</h1>
+      <div className="h-[500px] overflow-y-scroll">
+        <div className="border-2 p-3 rounded-lg mb-5 lg:flex">
+          <div className="relative lg:w-1/3">
+            <img src={MenuSatu} alt="" className="ms-4 w-80" />
+          </div>
+          <div className="lg:w-2/3 container mt-5 ">
+            <h1 className="text-xl font-semibold mb-3">Nasi Goreng</h1>
+            <div className="flex">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M2.25 18.75a60.07 60.07 0 0115.797 2.101c.727.198 1.453-.342 1.453-1.096V18.75M3.75 4.5v.75A.75.75 0 013 6h-.75m0 0v-.375c0-.621.504-1.125 1.125-1.125H20.25M2.25 6v9m18-10.5v.75c0 .414.336.75.75.75h.75m-1.5-1.5h.375c.621 0 1.125.504 1.125 1.125v9.75c0 .621-.504 1.125-1.125 1.125h-.375m1.5-1.5H21a.75.75 0 00-.75.75v.75m0 0H3.75m0 0h-.375a1.125 1.125 0 01-1.125-1.125V15m1.5 1.5v-.75A.75.75 0 003 15h-.75M15 10.5a3 3 0 11-6 0 3 3 0 016 0zm3 0h.008v.008H18V10.5zm-12 0h.008v.008H6V10.5z"
+                />
+              </svg>
+              <p className="ms-2 mb-3">Rp.40.000</p>
+            </div>
+            <div className="flex justify-start pe-5 font-bold text-yellow-500 text-md items-center">
+              <div className="flex">
+                <StarIcon className="h-7 w-7 text-yellow-500" />
+                <StarIcon className="h-7 w-7 text-yellow-500" />
+                <StarIcon className="h-7 w-7 text-yellow-500" />
+                <StarIcon className="h-7 w-7 text-yellow-500" />
+                <StarIcon className="h-7 w-7 text-gray-400" />
+              </div>
+              <p className="text-md ms-5">4.0/5.0</p>
+            </div>
+          </div>
+        </div>
+        <div className="border-2 p-3 rounded-lg mb-5 lg:flex">
+          <div className="relative lg:w-1/3">
+            <img src={MenuSatu} alt="" className="ms-4 w-80" />
+          </div>
+          <div className="lg:w-2/3 container mt-5 ">
+            <h1 className="text-xl font-semibold mb-3">Nasi Goreng</h1>
+            <div className="flex">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M2.25 18.75a60.07 60.07 0 0115.797 2.101c.727.198 1.453-.342 1.453-1.096V18.75M3.75 4.5v.75A.75.75 0 013 6h-.75m0 0v-.375c0-.621.504-1.125 1.125-1.125H20.25M2.25 6v9m18-10.5v.75c0 .414.336.75.75.75h.75m-1.5-1.5h.375c.621 0 1.125.504 1.125 1.125v9.75c0 .621-.504 1.125-1.125 1.125h-.375m1.5-1.5H21a.75.75 0 00-.75.75v.75m0 0H3.75m0 0h-.375a1.125 1.125 0 01-1.125-1.125V15m1.5 1.5v-.75A.75.75 0 003 15h-.75M15 10.5a3 3 0 11-6 0 3 3 0 016 0zm3 0h.008v.008H18V10.5zm-12 0h.008v.008H6V10.5z"
+                />
+              </svg>
+              <p className="ms-2 mb-3">Rp.40.000</p>
+            </div>
+            <div className="flex justify-start pe-5 font-bold text-yellow-500 text-md items-center">
+              <div className="flex">
+                <StarIcon className="h-7 w-7 text-yellow-500" />
+                <StarIcon className="h-7 w-7 text-yellow-500" />
+                <StarIcon className="h-7 w-7 text-yellow-500" />
+                <StarIcon className="h-7 w-7 text-yellow-500" />
+                <StarIcon className="h-7 w-7 text-gray-400" />
+              </div>
+              <p className="text-md ms-5">4.0/5.0</p>
+            </div>
+          </div>
+        </div>
+        <div className="border-2 p-3 rounded-lg mb-5 lg:flex">
+          <div className="relative lg:w-1/3">
+            <img src={MenuSatu} alt="" className="ms-4 w-80" />
+          </div>
+          <div className="lg:w-2/3 container mt-5 ">
+            <h1 className="text-xl font-semibold mb-3">Nasi Goreng</h1>
+            <div className="flex">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M2.25 18.75a60.07 60.07 0 0115.797 2.101c.727.198 1.453-.342 1.453-1.096V18.75M3.75 4.5v.75A.75.75 0 013 6h-.75m0 0v-.375c0-.621.504-1.125 1.125-1.125H20.25M2.25 6v9m18-10.5v.75c0 .414.336.75.75.75h.75m-1.5-1.5h.375c.621 0 1.125.504 1.125 1.125v9.75c0 .621-.504 1.125-1.125 1.125h-.375m1.5-1.5H21a.75.75 0 00-.75.75v.75m0 0H3.75m0 0h-.375a1.125 1.125 0 01-1.125-1.125V15m1.5 1.5v-.75A.75.75 0 003 15h-.75M15 10.5a3 3 0 11-6 0 3 3 0 016 0zm3 0h.008v.008H18V10.5zm-12 0h.008v.008H6V10.5z"
+                />
+              </svg>
+              <p className="ms-2 mb-3">Rp.40.000</p>
+            </div>
+            <div className="flex justify-start pe-5 font-bold text-yellow-500 text-md items-center">
+              <div className="flex">
+                <StarIcon className="h-7 w-7 text-yellow-500" />
+                <StarIcon className="h-7 w-7 text-yellow-500" />
+                <StarIcon className="h-7 w-7 text-yellow-500" />
+                <StarIcon className="h-7 w-7 text-yellow-500" />
+                <StarIcon className="h-7 w-7 text-gray-400" />
+              </div>
+              <p className="text-md ms-5">4.0/5.0</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default ProductLihatMenu;
--- src/pages/PageRoutes.jsx
+++ src/pages/PageRoutes.jsx
@@ -5,10 +5,12 @@ import HomePage from "./home/HomePage";
 import ProfilePage from "./profile/ProfilePage";
 import DetailPage from "./detail/DetailPage";
 import EditProfile from "./edit-profile/EditProfile";
+import LihatMenu from "./lihat-menu/LihatMenu";
 
 function PageRoutes() {
   return (
     <Routes>
+      <Route path="/" element={<LihatMenu />} />
       <Route path="/" element={<HomePage />} />
       <Route path="/" element={<DetailPage />} />
       <Route path="/" element={<ProfilePage />} />
